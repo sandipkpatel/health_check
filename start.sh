ef4bd7e3f401e4476dc6ceee805edc6ddee2de58
sandip@laitmatus.com <sandip@laitmatus.com>
2021-05-05 11:51:38 +0530

    heap size increased

--- start.sh
+++ start.sh
@@ -17,7 +17,7 @@ else
     echo "done"
 fi
 
-./furnishing -J-Xmx512M -J-Xms256M -Dhttp.port=$2 -Djavax.net.ssl.keyStore=/root/code/furnishing/ssl/a2032b2ccb8bab4189d8c32083fd8c3d.pfx -Djavax.net.ssl.keyStorePassword=furnishzing -Dplay.server.netty.maxInitialLineLength=1024000000 > /dev/null
+./furnishing -J-Xmx1000M -J-Xms1000M -Dhttp.port=$2 -Djavax.net.ssl.keyStore=/root/code/furnishing/ssl/a2032b2ccb8bab4189d8c32083fd8c3d.pfx -Djavax.net.ssl.keyStorePassword=furnishzing -Dplay.server.netty.maxInitialLineLength=1024000000 > /dev/null
 if [ $? != 0 ]
 then
     echo "failed!"
